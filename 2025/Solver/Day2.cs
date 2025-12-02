cc762e252fdbc0bfbd884dde8a9d05d5de0b3109
Ryan Lehan <RLehan@tql.com>
2025-12-02 17:31:49 -0500

    Day 2

---
+++ 2025/Solver/Day2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Solver;
+
+internal static class Day2
+{
+    public static long SumCountBadProductIdsPart1()
+    {
+        long sum = 0;
+
+        ProcessProductIds((id) =>
+        {
+            var idLen = id.Length;
+            if (idLen % 2 == 0 &&
+                id.Substring(0, idLen / 2) == id.Substring(idLen / 2))
+                sum += Int64.Parse(id);
+        });
+
+        return sum;
+    }
+
+    public static long SumCountBadProductIdsPart2()
+    {
+        long sum = 0;
+
+        ProcessProductIds((id) =>
+        {
+            var idLen = id.Length;
+            int maxGrabLen = idLen / 2;     // At most it can be 1/2 of the length of the text
+            bool isMatch = true;            // Assume we have a pattern match
+            int patternRepeatedCnt = 0;
+
+            for (int grabLen = 1; grabLen <= maxGrabLen; grabLen++)
+            {
+                isMatch = true;
+                patternRepeatedCnt = 1;                 // Account for 1st pattern match that we skip over in the loop
+                var pattern = id.Substring(0, grabLen);
+
+                // Check to make sure we can compare every part of the string
+                if (idLen % grabLen != 0) continue;
+
+                // Now compare pattern to rest of parts of string                
+                for (int i = grabLen; i <= idLen - grabLen; i+=grabLen)
+                {
+                    if (!pattern.Equals(id.Substring(i, grabLen)))
+                    {
+                        isMatch = false;
+                        break;
+                    }
+                    patternRepeatedCnt++;
+                }
+
+                // Break early if we have at least one complete pattern match
+                if (isMatch) break;
+            }
+
+            if (isMatch && patternRepeatedCnt > 1) sum += Int64.Parse(id);
+        });
+
+        return sum;
+    }
+
+
+    private static void ProcessProductIds(Action<string> function)
+    {
+        string productIds = File.ReadAllText("Day2ProductIDs.txt");
+        string[] productIdRanges = productIds.Split(',');
+        foreach (string idRange in productIdRanges)
+        {
+            var tempIds = idRange.Split('-');
+            long beginId = Int64.Parse(tempIds[0]);
+            long endId = Int64.Parse(tempIds[1]);
+
+            for (long id = beginId; id <= endId; id++)
+            {
+                function(id.ToString());
+            }
+        }
+    }
+}
--- 2025/Solver/Program.cs
+++ 2025/Solver/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Solver;
 
-Console.WriteLine($"Puzzle 1 Safe Password - Part One: {Day1.DecodePasswordPart1()}");
-Console.WriteLine($"Puzzle 1 Safe Password - Part Two: {Day1.DecodePasswordPart2()}");
+Console.WriteLine($"Day 1 Safe Password - Part One: {Day1.DecodePasswordPart1()}");
+Console.WriteLine($"Day 1 Safe Password - Part Two: {Day1.DecodePasswordPart2()}");
+
+Console.WriteLine($"Day 2 Incorrect Product ID - Part One: {Day2.SumCountBadProductIdsPart1()}");
+Console.WriteLine($"Day 2 Incorrect Product ID - Part Two: {Day2.SumCountBadProductIdsPart2()}");
